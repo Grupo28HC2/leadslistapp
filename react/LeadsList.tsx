917c2d18e5954f9f6520d56a2cc36e54f82c62ef
nyodinariai <nathan.nariai@gmail.com>
2021-08-29 15:52:42 -0300

    basicstyles added

--- react/LeadsList.tsx
+++ react/LeadsList.tsx
@@ -4,7 +4,11 @@ import { useState, useEffect } from 'react';
 
 import api from './api';
 
+import './styles/basicstyles'
+
 import 'vtex-tachyons'
+
+import { useCssHandles } from 'vtex.css-handles'
 
 
 interface LeadsList {}
@@ -18,9 +22,11 @@ interface Leads{
   prospect_date?: string
 }
 
-
 const LeadsList: StorefrontFunctionComponent<LeadsList> = ({}) => {
 
+  const CSS_HANDLES = ['container', 'tableBackground', 'tableTitle', 'tableContainer', 'tableHead', 'tableHeadRow','tableHeadDiv' ,'tableBody' ,'tableBodyRow', 'tableBodyDiv' ,'tableText', 'tableItem'] as const
+  
+  const handles = useCssHandles(CSS_HANDLES)  
 
   const [leads, setLeads] = useState<Leads[]>([]);
 
@@ -38,36 +44,37 @@ const LeadsList: StorefrontFunctionComponent<LeadsList> = ({}) => {
 
 
   return (
-      <div className="w-80 center pt4">
-        <div className="">
+      <div>
+        <div className={`${handles.container} w-80 center pt4`}>
+        <div className={handles.tableBackground}>
           <div className="">
           <div>
-            <h1 className="f1">Lista de Leads</h1>
+            <h1 className={`${handles.tableTitle} f1`}>Lista de Leads</h1>
           </div>
 
           <div className="">
-            <table className="f6 mb5 w-100 center">
-              <thead className="">
-                <tr className="stripe-dark">
-                <th className="fw6 tl bb b--black-20 pt2 bg-white">id</th>
-                <th className="fw6 tl bb b--black-20 pt2 bg-white">Nome</th>
-                <th className="fw6 tl bb b--black-20 pt2 bg-white">E-mail</th>
-                <th className="fw6 tl bb b--black-20 pt2 bg-white">Telefone</th>
-                <th className="fw6 tl bb b--black-20 pt2 bg-white">Data de Cadastro</th>
-                <th className="fw6 tl bb b--black-20 pt2 bg-white">Primeira Compra</th>
+            <table className={`${handles.tableContainer} f6 mb5 w-100 center`}>
+              <thead className={handles.tableHead}>
+                <tr className={`${handles.tableHeadRow} stripe-dark`}>
+                <th className={`${handles.tableHeadDiv} fw6 tl bb b--black-20 pt2 bg-white`}>id</th>
+                <th className={`${handles.tableHeadDiv} fw6 tl bb b--black-20 pt2 bg-white`}>Nome</th>
+                <th className={`${handles.tableHeadDiv} fw6 tl bb b--black-20 pt2 bg-white`}>E-mail</th>
+                <th className={`${handles.tableHeadDiv} fw6 tl bb b--black-20 pt2 bg-white`}>Telefone</th>
+                <th className={`${handles.tableHeadDiv} fw6 tl bb b--black-20 pt2 bg-white`}>Data de Cadastro</th>
+                <th className={`${handles.tableHeadDiv} fw6 tl bb b--black-20 pt2 bg-white`}>Primeira Compra</th>
                 </tr>
               </thead>
 
-              <tbody className="lh-copy">
+              <tbody className={`${handles.tableBody} lh-copy`}>
               {leads.length > 0 ?
                 leads.map(lead => (
-                  <tr className="stripe-dark bb b--black-20"  key={lead.email}>
-                    <td className="tl bb b--black-20 pt2">{lead.id}</td>
-                    <td className="tl bb b--black-20 pt2">{lead.name}</td>
-                    <td className="tl bb b--black-20 pt2">{lead.email}</td>
-                    <td className="tl bb b--black-20 pt2">{lead.telephone}</td>
-                    <td className="tl bb b--black-20 pt2">{lead.prospect_date === '' ? '-': lead.prospect_date}</td>
-                    <td className="tl bb b--black-20 pt2">{lead.customer_date === '' ? '-': lead.customer_date}</td>
+                  <tr className={`${handles.tableBodyRow}`}  key={lead.email}>
+                    <td className={`${handles.tableBodyDiv} tl bb b--black-20 pt2`}>{lead.id}</td>
+                    <td className={`${handles.tableBodyDiv} tl bb b--black-20 pt2`}>{lead.name}</td>
+                    <td className={`${handles.tableBodyDiv} tl bb b--black-20 pt2`}>{lead.email}</td>
+                    <td className={`${handles.tableBodyDiv} tl bb b--black-20 pt2`}>{lead.telephone}</td>
+                    <td className={`${handles.tableBodyDiv} tl bb b--black-20 pt2`}>{lead.prospect_date === '' ? '-': lead.prospect_date}</td>
+                    <td className={`${handles.tableBodyDiv} tl bb b--black-20 pt2`}>{lead.customer_date === '' ? '-': lead.customer_date}</td>
                   </tr>
                 )) : ''}
               </tbody>
@@ -75,7 +82,7 @@ const LeadsList: StorefrontFunctionComponent<LeadsList> = ({}) => {
           </div>
           </div>
         </div>
-
+        </div>
     </div>
   )}
 
